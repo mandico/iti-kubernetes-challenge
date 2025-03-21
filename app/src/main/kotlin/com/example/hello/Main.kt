28181dfbfbce80c7eb437390e78756f133c9fda5
Luiz Mandico <luizmandico@gmail.com>
2025-03-21 13:10:54 -0300

    Upgrade App e migração para container.

--- app/src/main/kotlin/com/example/hello/Main.kt
+++ app/src/main/kotlin/com/example/hello/Main.kt
@@ -1,19 +1,32 @@
 package com.example.hello
 
-import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.server.application.*
+import io.ktor.server.metrics.micrometer.*
+import io.micrometer.prometheus.*
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>) {
-    val target = System.getenv("TARGET") ?: "World"
-    val port = System.getenv("PORT") ?: "8080"
-    embeddedServer(Netty, port.toInt()) {
+fun main() {
+    // Cria o registry do Prometheus
+    val prometheusRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+
+    embeddedServer(Netty, port = 8080) {
+        // Instala o Micrometer Metrics no Ktor
+        install(MicrometerMetrics) {
+            registry = prometheusRegistry
+        }
+
         routing {
+            // Endpoint principal
             get("/") {
-                call.respondText("Hello $target!", ContentType.Text.Html)
+                call.respondText("Hello, World!")
+            }
+
+            // Endpoint para expor métricas do Prometheus
+            get("/actuator/prometheus") {
+                call.respondText(prometheusRegistry.scrape())
             }
         }
     }.start(wait = true)
